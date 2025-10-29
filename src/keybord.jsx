1e6d4af5814ee4d7215738c00699454a72d2b68b
tohargoren <tohargoren@hilma.tech>
2025-10-29 15:00:39 +0200

    keybord

--- src/keybord.jsx
+++ src/keybord.jsx
@@ -1,15 +1,31 @@
 import React, { useState } from "react";
-// import './App.css'
-function Keybord(){
-    const [inputValue, setInputValue] = useState("");
 
-  const handleChange = (e) => {
-    setInputValue(e.target.value);
+function Keybord(){
+  const [inputValue, setInputValue] = useState("");
+  const letters=["a","b","c","d","e","f","g","h","i","j","k","l","m", "n","o","p","q","r","s","t","u","v","w","x","y","z"," "];
+  const hebrewLetters = ["א","ב","ג","ד","ה","ו","ז","ח","ט","י",
+  "כ","ל","מ","נ","ס","ע","פ","צ","ק","ר","ש","ת"," "];
+
+
+  const handleChange = (event) => {
+    setInputValue(prev=> prev+event.target.value);
   };
 
+  
+  function CreateKeybord(arr){
+    return arr.map((item, index)=>(
+      <button key={index} value={item} onClick={handleChange}>{item}</button>
+    ));
+    }
+      
+  
   return (
     <div className="keybord">
-      <input type="text" onChange={handleChange} value={inputValue} />
+      <p>{inputValue}</p>
+      <div id="keybord">
+        {CreateKeybord(letters)}
+      </div>
+      {CreateKeybord(hebrewLetters)}
     </div>
   );
 
