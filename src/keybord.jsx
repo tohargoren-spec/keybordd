3049db2322d0b94c028db9d8cb87350d5613026a
noaeneyo <noaeneyo@hilma.tech>
2025-10-29 15:03:26 +0200

    dont save this

--- src/keybord.jsx
+++ src/keybord.jsx
@@ -1,15 +1,30 @@
 import React, { useState } from "react";
-// import './App.css'
+import './App.css'
+import alphabet from "./keys";
+
+
+
 function Keybord(){
-    const [inputValue, setInputValue] = useState("");
 
-  const handleChange = (e) => {
-    setInputValue(e.target.value);
-  };
-
+  const [words, setWords]= useState('')
+function addToString(letter){
+  setWords(prevWords => { return prevWords +letter})
+}
   return (
-    <div className="keybord">
-      <input type="text" onChange={handleChange} value={inputValue} />
+    <div className="App">
+      <h1>{words}</h1>
+
+        {alphabet.map((item, index) => (
+          <button onClick={() => addToString(index)} key={index} />
+          
+        ))}
+
+      <button onClick={()=>addToString('a')}>a</button>
+     <button onClick={()=>addToString('b')}>b</button>
+      <button onClick={()=>addToString('c')}>c</button>
+      <button onClick={()=>addToString('d')}>d</button>
+      <button onClick={()=>addToString('e')}>e</button>
+
     </div>
   );
 
